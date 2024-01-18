ff23edcb5aefc87f49d87bc8af98f8a51778e742
Marcelo Garrido <marcelogarrido@backmar.cl>
2024-01-18 13:29:21 -0300

    fix mes

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -22,18 +22,18 @@ class DataController extends Controller
             $mc = DB::table('data as d1')
             ->select(
                 'd1.nombre_sujeba',
-                DB::raw('SUM(CASE WHEN d1.mespro = 1 THEN d1.numero ELSE 0 END) as enero'),
-                DB::raw('SUM(CASE WHEN d1.mespro = 2 THEN d1.numero ELSE 0 END) as febrero'),
-                DB::raw('SUM(CASE WHEN d1.mespro = 3 THEN d1.numero ELSE 0 END) as marzo'),
-                DB::raw('SUM(CASE WHEN d1.mespro = 4 THEN d1.numero ELSE 0 END) as abril'),
-                DB::raw('SUM(CASE WHEN d1.mespro = 5 THEN d1.numero ELSE 0 END) as mayo'),
-                DB::raw('SUM(CASE WHEN d1.mespro = 6 THEN d1.numero ELSE 0 END) as junio'),
-                DB::raw('SUM(CASE WHEN d1.mespro = 7 THEN d1.numero ELSE 0 END) as julio'),
-                DB::raw('SUM(CASE WHEN d1.mespro = 8 THEN d1.numero ELSE 0 END) as agosto'),
-                DB::raw('SUM(CASE WHEN d1.mespro = 9 THEN d1.numero ELSE 0 END) as septiembre'),
-                DB::raw('SUM(CASE WHEN d1.mespro = 10 THEN d1.numero ELSE 0 END) as octubre'),
-                DB::raw('SUM(CASE WHEN d1.mespro = 11 THEN d1.numero ELSE 0 END) as noviembre'),
-                DB::raw('SUM(CASE WHEN d1.mespro = 12 THEN d1.numero ELSE 0 END) as diciembre'),
+                DB::raw('SUM(CASE WHEN MONTH(fecha) = 1 THEN d1.numero ELSE 0 END) as enero'),
+                DB::raw('SUM(CASE WHEN MONTH(fecha) = 2 THEN d1.numero ELSE 0 END) as febrero'),
+                DB::raw('SUM(CASE WHEN MONTH(fecha) = 3 THEN d1.numero ELSE 0 END) as marzo'),
+                DB::raw('SUM(CASE WHEN MONTH(fecha) = 4 THEN d1.numero ELSE 0 END) as abril'),
+                DB::raw('SUM(CASE WHEN MONTH(fecha) = 5 THEN d1.numero ELSE 0 END) as mayo'),
+                DB::raw('SUM(CASE WHEN MONTH(fecha) = 6 THEN d1.numero ELSE 0 END) as junio'),
+                DB::raw('SUM(CASE WHEN MONTH(fecha) = 7 THEN d1.numero ELSE 0 END) as julio'),
+                DB::raw('SUM(CASE WHEN MONTH(fecha) = 8 THEN d1.numero ELSE 0 END) as agosto'),
+                DB::raw('SUM(CASE WHEN MONTH(fecha) = 9 THEN d1.numero ELSE 0 END) as septiembre'),
+                DB::raw('SUM(CASE WHEN MONTH(fecha) = 10 THEN d1.numero ELSE 0 END) as octubre'),
+                DB::raw('SUM(CASE WHEN MONTH(fecha) = 11 THEN d1.numero ELSE 0 END) as noviembre'),
+                DB::raw('SUM(CASE WHEN MONTH(fecha) = 12 THEN d1.numero ELSE 0 END) as diciembre'),
                 DB::raw('SUM(d1.numero) as total')
             )
             ->where('d1.umd', '=', "M3")
@@ -46,18 +46,18 @@ class DataController extends Controller
             $km = DB::table('data as d1')
             ->select(
                 'd1.nombre_sujeba',
-                DB::raw('SUM(CASE WHEN d1.mespro = 1 THEN d1.numero ELSE 0 END) as enero'),
-                DB::raw('SUM(CASE WHEN d1.mespro = 2 THEN d1.numero ELSE 0 END) as febrero'),
-                DB::raw('SUM(CASE WHEN d1.mespro = 3 THEN d1.numero ELSE 0 END) as marzo'),
-                DB::raw('SUM(CASE WHEN d1.mespro = 4 THEN d1.numero ELSE 0 END) as abril'),
-                DB::raw('SUM(CASE WHEN d1.mespro = 5 THEN d1.numero ELSE 0 END) as mayo'),
-                DB::raw('SUM(CASE WHEN d1.mespro = 6 THEN d1.numero ELSE 0 END) as junio'),
-                DB::raw('SUM(CASE WHEN d1.mespro = 7 THEN d1.numero ELSE 0 END) as julio'),
-                DB::raw('SUM(CASE WHEN d1.mespro = 8 THEN d1.numero ELSE 0 END) as agosto'),
-                DB::raw('SUM(CASE WHEN d1.mespro = 9 THEN d1.numero ELSE 0 END) as septiembre'),
-                DB::raw('SUM(CASE WHEN d1.mespro = 10 THEN d1.numero ELSE 0 END) as octubre'),
-                DB::raw('SUM(CASE WHEN d1.mespro = 11 THEN d1.numero ELSE 0 END) as noviembre'),
-                DB::raw('SUM(CASE WHEN d1.mespro = 12 THEN d1.numero ELSE 0 END) as diciembre'),
+                DB::raw('SUM(CASE WHEN MONTH(fecha) = 1 THEN d1.numero ELSE 0 END) as enero'),
+                DB::raw('SUM(CASE WHEN MONTH(fecha) = 2 THEN d1.numero ELSE 0 END) as febrero'),
+                DB::raw('SUM(CASE WHEN MONTH(fecha) = 3 THEN d1.numero ELSE 0 END) as marzo'),
+                DB::raw('SUM(CASE WHEN MONTH(fecha) = 4 THEN d1.numero ELSE 0 END) as abril'),
+                DB::raw('SUM(CASE WHEN MONTH(fecha) = 5 THEN d1.numero ELSE 0 END) as mayo'),
+                DB::raw('SUM(CASE WHEN MONTH(fecha) = 6 THEN d1.numero ELSE 0 END) as junio'),
+                DB::raw('SUM(CASE WHEN MONTH(fecha) = 7 THEN d1.numero ELSE 0 END) as julio'),
+                DB::raw('SUM(CASE WHEN MONTH(fecha) = 8 THEN d1.numero ELSE 0 END) as agosto'),
+                DB::raw('SUM(CASE WHEN MONTH(fecha) = 9 THEN d1.numero ELSE 0 END) as septiembre'),
+                DB::raw('SUM(CASE WHEN MONTH(fecha) = 10 THEN d1.numero ELSE 0 END) as octubre'),
+                DB::raw('SUM(CASE WHEN MONTH(fecha) = 11 THEN d1.numero ELSE 0 END) as noviembre'),
+                DB::raw('SUM(CASE WHEN MONTH(fecha) = 12 THEN d1.numero ELSE 0 END) as diciembre'),
                 DB::raw('SUM(d1.numero) as total')
             )
             ->where('d1.umd', '=', "KM")
@@ -69,18 +69,18 @@ class DataController extends Controller
         $results = DB::table('data as d1')
             ->select(
                 'd1.nombre_sujeba',
-                DB::raw('SUM(CASE WHEN d1.mespro = 1 THEN d1.totpes ELSE 0 END) as enero'),
-                DB::raw('SUM(CASE WHEN d1.mespro = 2 THEN d1.totpes ELSE 0 END) as febrero'),
-                DB::raw('SUM(CASE WHEN d1.mespro = 3 THEN d1.totpes ELSE 0 END) as marzo'),
-                DB::raw('SUM(CASE WHEN d1.mespro = 4 THEN d1.totpes ELSE 0 END) as abril'),
-                DB::raw('SUM(CASE WHEN d1.mespro = 5 THEN d1.totpes ELSE 0 END) as mayo'),
-                DB::raw('SUM(CASE WHEN d1.mespro = 6 THEN d1.totpes ELSE 0 END) as junio'),
-                DB::raw('SUM(CASE WHEN d1.mespro = 7 THEN d1.totpes ELSE 0 END) as julio'),
-                DB::raw('SUM(CASE WHEN d1.mespro = 8 THEN d1.totpes ELSE 0 END) as agosto'),
-                DB::raw('SUM(CASE WHEN d1.mespro = 9 THEN d1.totpes ELSE 0 END) as septiembre'),
-                DB::raw('SUM(CASE WHEN d1.mespro = 10 THEN d1.totpes ELSE 0 END) as octubre'),
-                DB::raw('SUM(CASE WHEN d1.mespro = 11 THEN d1.totpes ELSE 0 END) as noviembre'),
-                DB::raw('SUM(CASE WHEN d1.mespro = 12 THEN d1.totpes ELSE 0 END) as diciembre'),
+                DB::raw('SUM(CASE WHEN MONTH(fecha) = 1 THEN d1.totpes ELSE 0 END) as enero'),
+                DB::raw('SUM(CASE WHEN MONTH(fecha) = 2 THEN d1.totpes ELSE 0 END) as febrero'),
+                DB::raw('SUM(CASE WHEN MONTH(fecha) = 3 THEN d1.totpes ELSE 0 END) as marzo'),
+                DB::raw('SUM(CASE WHEN MONTH(fecha) = 4 THEN d1.totpes ELSE 0 END) as abril'),
+                DB::raw('SUM(CASE WHEN MONTH(fecha) = 5 THEN d1.totpes ELSE 0 END) as mayo'),
+                DB::raw('SUM(CASE WHEN MONTH(fecha) = 6 THEN d1.totpes ELSE 0 END) as junio'),
+                DB::raw('SUM(CASE WHEN MONTH(fecha) = 7 THEN d1.totpes ELSE 0 END) as julio'),
+                DB::raw('SUM(CASE WHEN MONTH(fecha) = 8 THEN d1.totpes ELSE 0 END) as agosto'),
+                DB::raw('SUM(CASE WHEN MONTH(fecha) = 9 THEN d1.totpes ELSE 0 END) as septiembre'),
+                DB::raw('SUM(CASE WHEN MONTH(fecha) = 10 THEN d1.totpes ELSE 0 END) as octubre'),
+                DB::raw('SUM(CASE WHEN MONTH(fecha) = 11 THEN d1.totpes ELSE 0 END) as noviembre'),
+                DB::raw('SUM(CASE WHEN MONTH(fecha) = 12 THEN d1.totpes ELSE 0 END) as diciembre'),
                 DB::raw('SUM(d1.totpes) as total')
             )
             ->where('d1.anopro', '=', 2023)
@@ -89,7 +89,7 @@ class DataController extends Controller
 
             ->get();
 
-        return view('welcome', ['data' => $km]);
+        return view('welcome', ['data' => $results]);
     }
 
     /**
